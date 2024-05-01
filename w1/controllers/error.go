e924280c3a5194c4c552d3e31b5b2d0e09e02164
enjaytarigan <tariganagc@gmail.com>
2024-05-01 18:24:48 +0700

    refactor: add error log

--- w1/controllers/error.go
+++ w1/controllers/error.go
@@ -1,7 +1,11 @@
 package controllers
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"runtime/debug"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
@@ -18,14 +22,22 @@ func handleError(c *gin.Context, err error) {
 	// go-validator
 	if validationErrors, ok := err.(validator.ValidationErrors); ok && len(validationErrors) > 0 {
 		code = http.StatusBadRequest
-		message = err.Error()
 	}
 
 	// Domain Error
 	pr, ok := err.(httpStatusCodeProvider)
 	if ok {
 		code = pr.HTTPStatusCode()
-		message = err.Error()
+	}
+
+	if code == http.StatusInternalServerError {
+		errLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+		trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
+
+		errLog.Output(2, trace)
+
+		message = "Internal Server Error"
 	}
 
 	c.JSON(code, gin.H{
