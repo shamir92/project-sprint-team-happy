879dc94bd066c87c4cdff8b8a2c118f1b96fa779
enjaytarigan <tariganagc@gmail.com>
2024-05-08 06:31:40 +0700

    feat: use env for connection string

--- w2/cmd/api/main.go
+++ w2/cmd/api/main.go
@@ -3,13 +3,30 @@ package main
 import (
 	"eniqlostore/internal/postgres"
 	httpserver "eniqlostore/server/http"
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
+func connectionString() string {
+	dbUsername := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+	dbParams := os.Getenv("DB_PARAMS")
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?%s", dbUsername, dbPassword, dbHost, dbPort, dbName, dbParams)
+
+	return connStr
+}
+
 func main() {
-	db, err := postgres.NewDB("postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable")
+	godotenv.Load()
+
+	db, err := postgres.NewDB(connectionString())
 
 	if err != nil {
 		log.Fatalf("database failed to open: %v", err)
