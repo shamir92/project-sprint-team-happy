447f1af805c70f45a1826f7e67e1e7930171fecd
Maulana Zain <maulanadezain@gmail.com>
2024-05-14 10:34:15 +0700

    feat(w3-migrations): translate erd to sql

---
+++ w3/migrations/20240513155045_medical_records.up.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS public.medical_records(
+    id serial NOT NULL,
+    patient_id character(16) NOT NULL,
+    symtomps character varying(2000) NOT NULL,
+    medications character varying(2000) NOT NULL,
+    created_by uuid NOT NULL,
+    created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT medical_records_pkey PRIMARY KEY (id),
+    CONSTRAINT medical_records_patient_id_fk 
+        FOREIGN KEY (patient_id) 
+        REFERENCES public.patients(id)
+        ON DELETE CASCADE,
+    CONSTRAINT medical_records_created_by_fk 
+        FOREIGN KEY (created_by) 
+        REFERENCES public.users(id)
+        ON DELETE CASCADE
+)
+
+CREATE INDEX medical_records_patient_id_idx ON medical_records (patient_id)
+CREATE INDEX medical_records_created_by_idx ON medical_records (created_by)
