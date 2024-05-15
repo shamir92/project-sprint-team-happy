6be876da8b1c3d560fa7461b1ffd4b3de2560b6c
Shamir Husein <shamirhusein@gmail.com>
2024-05-15 13:33:29 +0700

    feat/authentication-login

--- w3/domain/usecase/user_it_usecase.go
+++ w3/domain/usecase/user_it_usecase.go
@@ -15,6 +15,7 @@ type userITUsecase struct {
 
 type IUserITUsecase interface {
 	RegisterUserIT(userITRequest UserITRegisterRequest) (UserITRegisterResponse, error)
+	LoginUserIT(request UserITLoginRequest) (UserITLoginResponse, error)
 }
 
 func NewUserITUsecase(bcryptHelper helper.IBcryptPasswordHash, userRepository repository.IUserRepository, jwtManager helper.IJWTManager) *userITUsecase {
@@ -71,6 +72,51 @@ func (u *userITUsecase) RegisterUserIT(userITRequest UserITRegisterRequest) (Use
 		return UserITRegisterResponse{}, err
 	}
 	return UserITRegisterResponse{
+		Token: token,
+		ID:    user.ID.String(),
+		Name:  user.Name,
+		NIP:   user.NIP,
+	}, nil
+}
+
+type UserITLoginRequest struct {
+	NIP      int    `json:"nip" validate:"required,numeric,min=6150000000000,max=6159999999999"`
+	Password string `json:"password" validate:"required,min=5,max=33"`
+}
+
+type UserITLoginResponse struct {
+	Token string `json:"access_token"`
+	ID    string `json:"user_id"`
+	Name  string `json:"name"`
+	NIP   string `json:"nip"`
+}
+
+func (u *userITUsecase) LoginUserIT(request UserITLoginRequest) (UserITLoginResponse, error) {
+	var user entity.User
+
+	userNip := strconv.FormatInt(int64(request.NIP), 10)
+	if !user.ValidateNIP(userNip, entity.IT) {
+		return UserITLoginResponse{}, helper.CustomError{
+			Message: "NIP is not valid",
+			Code:    400,
+		}
+	}
+	user, err := u.userRepository.GetByNIP(userNip)
+	if err != nil {
+		return UserITLoginResponse{}, err
+	}
+	if !u.bcryptHelper.Compare(user.Password, request.Password) {
+		return UserITLoginResponse{}, helper.CustomError{
+			Message: "password is not valid",
+			Code:    400,
+		}
+	}
+
+	token, err := u.jwtManager.CreateToken(user)
+	if err != nil {
+		return UserITLoginResponse{}, err
+	}
+	return UserITLoginResponse{
 		Token: token,
 		ID:    user.ID.String(),
 		Name:  user.Name,
--- w3/protocol/api/controller/user_it_controller.go
+++ w3/protocol/api/controller/user_it_controller.go
@@ -5,6 +5,7 @@ import (
 
 	"halosuster/domain/usecase"
 	"halosuster/internal/helper"
+	"halosuster/protocol/api/dto"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -17,17 +18,13 @@ type userITController struct {
 // TODO: add all function under ping controller to inferface. this will make it easier to test
 type IUserITController interface {
 	RegisterUserIT(c *fiber.Ctx) error
+	LoginUserIT(c *fiber.Ctx) error
 }
 
 func NewUserITController(userITUsecase usecase.IUserITUsecase) *userITController {
 	return &userITController{
 		userITUsecase: userITUsecase,
 	}
-}
-
-type UserITRegisterControllerResponse struct {
-	Message string `json:"message"`
-	Data    any    `json:"data"`
 }
 
 func (pc *userITController) RegisterUserIT(c *fiber.Ctx) error {
@@ -57,8 +54,41 @@ func (pc *userITController) RegisterUserIT(c *fiber.Ctx) error {
 	}
 	// TODO: return response
 
-	return c.Status(http.StatusCreated).JSON(UserITRegisterControllerResponse{
+	return c.Status(http.StatusCreated).JSON(dto.UserITRegisterControllerResponse{
 		Message: "user registered successfully",
+		Data:    data,
+	})
+}
+
+func (pc *userITController) LoginUserIT(c *fiber.Ctx) error {
+	//TODO: initization variable
+	var request usecase.UserITLoginRequest
+
+	// parse json
+	if err := c.BodyParser(&request); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Cannot parse JSON",
+		})
+	}
+
+	// TODO: Validate Struct
+	if err := helper.ValidateStruct(&request); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	// TODO: add logic
+	data, err := pc.userITUsecase.LoginUserIT(request)
+	if err != nil {
+		// tar ganti
+		// dirty dulu.
+		return c.Status(http.StatusInternalServerError).JSON(err)
+	}
+	// TODO: return response
+
+	return c.Status(http.StatusCreated).JSON(dto.UserITRegisterControllerResponse{
+		Message: "user login successfully",
 		Data:    data,
 	})
 }
