e38c667b9e309e00e049d19298907f33c024bade
Shamir Husein <shamirhusein@gmail.com>
2024-05-19 16:00:56 +0700

    berantem dengan k6

--- w3/protocol/api/cmd/main.go
+++ w3/protocol/api/cmd/main.go
@@ -14,7 +14,7 @@ import (
 
 func main() {
 	if err := godotenv.Load(); err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("error loading .env file")
 	}
 	// For configuration
 	var appConfiguration configuration.IAppConfiguration = configuration.NewAppConfiguration()
