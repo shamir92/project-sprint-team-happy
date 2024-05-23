879d1bd03a0db6070188dc8c6cbde1171664a093
Maulana Zain <maulanadezain@gmail.com>
2024-05-23 12:47:56 +0700

    feat(w4-migrations): add order entity

---
+++ w4/domain/entity/order_entity.go
@@ -0,0 +1,29 @@
+package entity
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type OrderState string
+
+const (
+	Estimated OrderState = "estimated"
+	Ordered   OrderState = "ordered"
+)
+
+func (os OrderState) String() string {
+	return string(os)
+}
+
+type Order struct {
+	ID                    uuid.UUID
+	UserID                uuid.UUID
+	UserLat               float64
+	UserLon               float64
+	TotalPrice            int
+	EstimatedDeliveryTime int
+	State                 string
+	CreatedAt             time.Time
+}
